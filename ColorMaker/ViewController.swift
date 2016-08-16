a516c98ee48b0e6732c50417a2f419e5e5f688cf
David Gibbs <david.gibbs@dressipi.com>
2016-08-16 22:20:46 +0100

    Add RGB value label and functionality + new app icons

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -15,6 +16,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var rgbValueLabel: UILabel!
+    
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        rgbValueLabel.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        rgbValueLabel.layer.cornerRadius = 20.0
+        rgbValueLabel.clipsToBounds = true
+    }
     
     // MARK: Actions
     @IBAction func moveSlider(sender: UISlider) {
@@ -23,11 +36,7 @@ class ViewController: UIViewController {
     
     func updateColorView(red r: CGFloat, green g: CGFloat, blue b: CGFloat) {
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        rgbValueLabel.text = "RGB: \(Int(r * 255)) / \(Int(g * 255)) / \(Int(b * 255))"
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
 }
 
